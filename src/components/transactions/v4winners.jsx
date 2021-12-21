f7bed1565dea8226213f33bb1fe0e3968ff3e5a4
underethsea <underthesea.pool@gmail.com>
2021-12-20 21:25:16 -0800

    add modal

--- src/components/transactions/v4winners.jsx
+++ src/components/transactions/v4winners.jsx
@@ -1,196 +1,241 @@
-
-
-import React, {useState, useEffect} from 'react'
-import axios from 'axios'
-import { ethers } from "ethers"
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { ethers } from "ethers";
 import Select from "react-select";
 
-// import { TimeConvert } from './timeConvert.js'
+import "./../modal.css";
 
-const chains = [1,137]
+// import { TimeConvert } from './timeConvert.js'
+console.log("start page");
+const chains = [1, 137];
 function separator(numb) {
-    var str = numb.toString().split(".");
-    str[0] = str[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    return str.join(".");
+  var str = numb.toString().split(".");
+  str[0] = str[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return str.join(".");
 }
 
 function amount(weiAmount) {
-     weiAmount = parseFloat(weiAmount);
-    weiAmount = weiAmount / 1000000;
-    return weiAmount.toFixed(0);
-      }
-      
-      const emoji = (amount) => {
-        let emojiIcon = "";
-        let weiAmount = amount / 1000000;
-        amount = parseFloat(weiAmount);
-        if(amount > 2499) 
-            {emojiIcon = "whale"} else
-        if(amount > 499)
-            {emojiIcon = "dolphin"} else
-        if(amount > 199)
-            {emojiIcon = "octopus"} else
-        if(amount > 99)
-            {emojiIcon = "lobster"} else
-        if(amount > 9)
-            {emojiIcon = "fis"} else
-            {emojiIcon = "fish"}
+  weiAmount = parseFloat(weiAmount);
+  weiAmount = weiAmount / 1000000;
+  return weiAmount.toFixed(0);
+}
+
+var textArray = [
+    'Searching for prizes. Lots of prizes...',
+    'How many prizes can there be?!? Still looking... ',
+    "Our servers may be overheating from prize search... Please hold",
+    "Can there really be thousands of prizes per day?!?!  BRB, double checking..."
+];
+
+function modalText(){
+    var randomNumber = Math.floor(Math.random()*textArray.length);
+    return textArray[randomNumber];
+}
 
 
+const emoji = (amount) => {
+  let emojiIcon = "";
+  let weiAmount = amount / 1000000;
+  amount = parseFloat(weiAmount);
+  if (amount > 2499) {
+    emojiIcon = "whale";
+  } else if (amount > 499) {
+    emojiIcon = "dolphin";
+  } else if (amount > 199) {
+    emojiIcon = "octopus";
+  } else if (amount > 99) {
+    emojiIcon = "lobster";
+  } else if (amount > 9) {
+    emojiIcon = "fis";
+  } else {
+    emojiIcon = "fish";
+  }
 
-        return emojiIcon;
-    }
+  return emojiIcon;
+};
 
 function transactionString(transString) {
-    return transString.replace(transString.substring(4,56), "....");
+  return transString.replace(transString.substring(4, 56), "....");
 }
 function addressString(addString) {
-    return addString.substring(0,10);
+  return addString.substring(0, 10);
 }
 const drawings = 65;
-function UsdcWinners() {
-   
-let options = []
-    for (let i = 0; i <= drawings; i++) {
-        options.push({label: i,value: i})
-      }
-  options.reverse();
-    const startDraw = {label: drawings,value: drawings};
-    const [transactions, setTransactions] = useState([]);
-    const [median, setMedian] = useState([]);
-    const [draw, setDraw] = useState(startDraw);
-    const onChange = selectedOption => {
-        setDraw(selectedOption);
-        console.log(`Option selected:`, selectedOption);
-      };
-    
-useEffect(() => {
-  
-    const goooo = async () => {
-        console.log(draw);
-        // let [polyApi,apiMainnet] = Promise.all(x)
-        const headers = {
-      headers : { 
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-       }}
-        let polyApi = "/api/prizes/137/0x8141bcfbcee654c5de17c4e2b2af26b67f9b9056/draw/" + draw.value + "/prizes.json"
-        let mainApi = "/api/prizes/1/0xb9a179dca5a7bf5f8b9e088437b3a85ebb495efe/draw/" + draw.value + "/prizes.json"
-  console.log(polyApi)
-        let [api, apiMainnet] = await Promise.all([
-            fetch(polyApi,headers),
-            fetch(mainApi,headers)
-            
-            ]);
-                                        
-//         let api = await fetch(polyApi,{
-//       headers : { 
-//         'Content-Type': 'application/json',
-//         'Accept': 'application/json'
-//        }})
-        console.log(api)
-api = await api.json();
-api.forEach((v) => {
-  v.chain = "Polygon";
-});
-//     let apiMainnet = await fetch("/api/prizes/1/0xb9a179dca5a7bf5f8b9e088437b3a85ebb495efe/draw/" + draw.value + "/prizes.json", })
-      let apiMain = await apiMainnet.json()
-      apiMain.forEach((v) => {
-        v.chain = "Ethereum";
-      });
-    let result = api.concat(apiMain);
 
-    let events = result;
-   
-    events.sort(function (a, b) {
-        return a.amount - b.amount;
-
-    });
-      events.reverse();
-      let key = 0;
-      api.forEach((v) => {
-        v.key = key;
-        key++;
-      });
-      
-      setTransactions(events);
+let options = [];
+for (let i = 0; i <= drawings; i++) {
+  options.push({ label: i, value: i });
 }
-goooo();
+options.reverse();
 
+function UsdcWinners() {
+  const [transactions, setTransactions] = useState([]);
+  const [total, setTotal] = useState([]);
+  const [depositors, setDepositors] = useState([]);
 
+  const [popup, setPopup] = useState(Boolean);
+  const onChange = (selectedOption) => {
+    setDraw(selectedOption);
+    console.log(`Option selected:`, selectedOption);
+  };
+  const startDraw = { label: drawings, value: drawings };
+  const [draw, setDraw] = useState(startDraw);
+
+  useEffect(() => {
+    const goooo = async () => {
+      setPopup(true);
+      console.log(draw);
+      // let [polyApi,apiMainnet] = Promise.all(x)
+      const headers = {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      };
+      // "https://thingproxy.freeboard.io/fetch/https://api.pooltogether.com/"
+      let polyApi = "prizes/137/0x8141bcfbcee654c5de17c4e2b2af26b67f9b9056/draw/" +
+        draw.value +
+        "/prizes.json";
+      let mainApi ="prizes/1/0xb9a179dca5a7bf5f8b9e088437b3a85ebb495efe/draw/" +
+        draw.value +
+        "/prizes.json";
+      console.log(polyApi);
+
+      let [api, apiMainnet] = await Promise.all([
+        fetch(polyApi, headers),
+        fetch(mainApi, headers),
+      ]);
+
+      console.log(api);
+    //   api = await api.json();
+      let [apiPolyJson, apiMainJson] = await Promise.all([
+          api.json(),
+          apiMainnet.json()
+      ])
+      apiPolyJson.forEach((v) => {
+        v.chain = "137";
+      });
+    //   let apiMain = await apiMainnet.json();
+      
+      apiMainJson.forEach((v) => {
+        v.chain = "1";
+      });
+      let events = apiPolyJson.concat(apiMainJson);
+
+      events.sort(function (a, b) {
+        return a.amount - b.amount;
+      });
+      events.reverse();
+
+      setTransactions(events);
+      setPopup(false);
+    };
+    goooo();
+
+  setDepositors(transactions.length);
+  let totals = 0;
+  let balance = 0;
+  transactions.forEach((item) => {
+    balance = parseFloat(amount(item.amount));
+    totals +=  balance;
+});
+setTotal(totals)
+  }, [draw]);
   
-
-}, [draw]);
-let depositors = transactions.length;
-let total = 0;
-    let balance = 0;
-    transactions.forEach((item) => {
-        balance = parseFloat(amount(item.amount));
-        total = total + balance;
-    })
-
-    let average = total / depositors;
-    
-return (
-    <div className = "transactions section">
-        {/* <button onClick={() => setDraw(58)}>Draw 58</button><button onClick={() => setDraw(59)}>Draw 59</button><button onClick={() => setDraw(60)}>Draw 60</button> */}
-        <div className = "card has-table has-mobile-sort-spaced">
-            <header className = "card-header">
-                <p className = "card-header-title">
-                DRAW #&nbsp;<Select options={options} onChange={onChange} value={draw}/>&nbsp;
-                WINNERS&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <span className="numb-purp"> {separator(depositors)}</span> PRIZES 
-                <img src='./images/usdc.png' className='token'/> 
-                <span className="numb-purp">{separator(total)}</span> TOTAL
-
-                {/* <img src='./images/usdc.png' className='token'/>
+  return (
+    <div className="transactions section">
+      {/* <button onClick={() => setDraw(58)}>Draw 58</button><button onClick={() => setDraw(59)}>Draw 59</button><button onClick={() => setDraw(60)}>Draw 60</button> */}
+      <div className="card has-table has-mobile-sort-spaced">
+        <header className="card-header">
+          {popup && (
+            <div className="modal">
+              <div className="modal-content">
+                  <center>
+                <p>{modalText()}</p>
+                </center>
+              </div>
+            </div>
+          )}
+          <p className="card-header-title">
+            DRAW #&nbsp;
+            <Select options={options} onChange={onChange} value={draw} />
+            &nbsp; WINNERS&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <span className="numb-purp"> {separator(depositors)}</span> PRIZES
+            <img src="./images/usdc.png" className="token" />
+            <span className="numb-purp">{separator(total)}</span> TOTAL
+            {/* <img src='./images/usdc.png' className='token'/>
                 <span className="numb-purp">{separator(average.toFixed(0))}</span>
                 &nbsp;AVG 
                 <img src='./images/usdc.png' className='token'/>
                 <span className="numb-purp">{separator(median)}</span>
                 &nbsp;MEDIAN */}
-                </p>
-            </header>
-            <div className="card-content">
-                <div className="table-wrapper has-mobile-cards">
-                    <table className="is-stripped table is-hoverable">
-                        <thead>
-                            <tr>
-                                {/* <th>Transaction Hash</th>
+          </p>
+        </header>
+        <div className="card-content">
+          <div className="table-wrapper has-mobile-cards">
+            <table className="is-stripped table is-hoverable">
+              <thead>
+                <tr>
+                  {/* <th>Transaction Hash</th>
                                 <th>Time</th> */}
-                                <th>Address</th>
-                                <th>Amount</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        {transactions.map((item) => (
-                            <tr key={item}>
-                                {/* <td></td>  <td></td> */}
-                                <td>{item.chain == "Ethereum" ? <span><img src="./images/ethereum.png" className="emoji"/> &nbsp;&nbsp;
-                                <a href={'https://etherscan.io/address/' + item.address} target='_blank' rel="noopener noreferrer">
-                                {item.address} 
-                            </a></span>
-                                :<span><img src="./images/polygon.png" className="emoji"/>
-                                &nbsp;&nbsp;
-                                <a href={'https://polygonscan.com/address/' + item.address} target='_blank' rel="noopener noreferrer">
-                                {item.address} 
-                            </a></span>}
-                                    
-                                    {/* &nbsp;{checkIfYearnOrPod(item.address)} */}
-                                    </td>
-                                <td>
-                                <img src={'./images/' + emoji(item.amount) + '.png'} className='emoji' alt='emoji'></img>
-                                &nbsp;
-                                    {separator(amount(item.amount))}
-                                    </td>
-                            </tr>
-                        ))}
-                            </tbody></table>
+                  <th>Address</th>
+                  <th>Amount</th>
+                </tr>
+              </thead>
+              <tbody>
+                {transactions.map((item) => (
+                  <tr>
+                    {/* <td></td>  <td></td> */}
+                    <td>
+                      {item.chain == "1" ? (
+                        <span>
+                          <img src="./images/ethereum.png" className="emoji" />{" "}
+                          &nbsp;&nbsp;
+                          <a
+                            href={
+                              "https://etherscan.io/address/" + item.address
+                            }
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
+                            {item.address}
+                          </a>
+                        </span>
+                      ) : (
+                        <span>
+                          <img src="./images/polygon.png" className="emoji" />
+                          &nbsp;&nbsp;
+                          <a
+                            href={
+                              "https://polygonscan.com/address/" + item.address
+                            }
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
+                            {item.address}
+                          </a>
+                        </span>
+                      )}
 
-                    </div></div>
-            </div>
+                      {/* &nbsp;{checkIfYearnOrPod(item.address)} */}
+                    </td>
+                    <td>
+                      <img
+                        src={"./images/" + emoji(item.amount) + ".png"}
+                        className="emoji"
+                        alt="emoji"
+                      ></img>
+                      &nbsp;
+                      {separator(amount(item.amount))}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
-
-)
+      </div>
+    </div>
+  );
 }
-export default UsdcWinners;    
+export default UsdcWinners;
