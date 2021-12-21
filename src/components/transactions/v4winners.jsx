9c6cd1d9685538a40b980422a587fa0c0d8243d0
underethsea <90292032+underethsea@users.noreply.github.com>
2021-12-20 16:01:01 -0800

    Update v4winners.jsx

--- src/components/transactions/v4winners.jsx
+++ src/components/transactions/v4winners.jsx
@@ -69,11 +69,9 @@ useEffect(() => {
     const goooo = async () => {
         console.log("loading drawing ",draw);
 
-        let polyApi = "https://thingproxy.freeboard.io/fetch/" + 
-        "https://api.pooltogether.com/prizes/137/0x8141bcfbcee654c5de17c4e2b2af26b67f9b9056/draw/" + 
+        let polyApi = "https://api.pooltogether.com/prizes/137/0x8141bcfbcee654c5de17c4e2b2af26b67f9b9056/draw/" + 
         draw.value + "/prizes.json"
-        let mainApi= "https://thingproxy.freeboard.io/fetch/" + 
-        "https://api.pooltogether.com/prizes/1/0xb9a179dca5a7bf5f8b9e088437b3a85ebb495efe/draw/" 
+        let mainApi= "https://api.pooltogether.com/prizes/1/0xb9a179dca5a7bf5f8b9e088437b3a85ebb495efe/draw/" 
         + draw.value + "/prizes.json"
 
         let [api, apiMain]= await Promise.all([
