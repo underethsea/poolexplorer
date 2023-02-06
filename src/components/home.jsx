d8eff4104e5389e5215e6e9814d3c31cfb4998b9
underethsea <underethsea@gmail.com>
2023-02-05 21:01:44 -0800

    stats

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -17,8 +17,9 @@ function Home() {
 
       try {
 
-        let stats = await getStats();
-    setStats(stats)
+        let statsGot = await getStats();
+        
+    setStats(statsGot)
       } catch (e) {
         console.log("fetch error i ", e);
       }
@@ -52,12 +53,12 @@ function Home() {
             <img src="./images/poolers.svg" />
           </span>
           {/* <span className="home-poolers-title">Total Poolers</span><br></br> */}
-          <span className="home-poolers-value">{separator(stats?.totalPlayers)}</span>
+          <span className="home-poolers-value">{separator(parseInt(stats.totalPlayers))}</span>
         </span>
         &nbsp;&nbsp;&nbsp;
         <span className="home-poolers-data">
           <span className="home-poolers-svg-box">
-        <img src="./images/tvl.svg" /></span><span className="home-poolers-value"></span></span>
+        <img src="./images/tvl.svg" /></span><span className="home-poolers-value">{separator(parseInt(stats.tvl.total))}</span></span>
       </div>
       <br></br>
       <div className="recent-lucky-toggle black-text">
@@ -72,7 +73,7 @@ function Home() {
       <div className="recent-lucky-toggle black-text">
 
       <img src="./images/pool.png" className="token"/>&nbsp; POOL - ${stats?.pool.toFixed(2)}
-      <img src="./images/optimism.png" className="token"/>&nbsp; OP APR - 8.9%&nbsp;&nbsp;&nbsp;&nbsp;</div>
+      <img src="./images/optimism.png" className="token"/>&nbsp; OP APR - {(((20000 * stats.op * 52) / stats.tvl.optimism )*100).toFixed(2)}% &nbsp;&nbsp;&nbsp;&nbsp;</div>
       {recent === "recent-on" && (
         <div>
           <UsdcWinners short={true} />
