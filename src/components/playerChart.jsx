7ffebc14c5da507535be57a8339e563df95cf7cb
underethsea <underethsea@gmail.com>
2023-04-27 16:24:07 -0700

    gradiate

--- src/components/playerChart.jsx
+++ src/components/playerChart.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import { LineChart, Area, AreaChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
 const formatBalance = (balance) => {
     if (balance >= 1000000) {
@@ -41,7 +41,6 @@ const PlayerChart = ({ address }) => {
     const domainMin = Math.ceil(minBalance-(minBalance*.05))
     return [{ data, domainMax , domainMin}];
   };
-  
 
   const renderCharts = () => {
     const formattedStats = formatStats();
@@ -49,59 +48,62 @@ const PlayerChart = ({ address }) => {
       return (
         <div>
          Balance History
-          <LineChart
-            width={300}
-            height={225}
-            data={network.data}
-            margin={{ top: 5, right: 10, left: 0, bottom: 5 }}
-            padding={{ top: 40, right: 40, bottom: 4, left: 4 }}
+         <AreaChart
+  width={300}
+  height={225}
+  data={network.data}
+  margin={{ top: 5, right: 10, left: 0, bottom: 5 }}
+  padding={{ top: 40, right: 40, bottom: 4, left: 4 }}
+  style={{ backgroundColor: "black" }}
+>
+  <defs>
+    <linearGradient id="areaGradient" x1="0" y1="0" x2="0" y2="1">
+      <stop offset="5%" stopColor="#ff4fff" stopOpacity={0.8} />
+      <stop offset="55%" stopColor="#ff4fff" stopOpacity={0} />
+    </linearGradient>
+  </defs>
+  <XAxis
+    width={50}
+    dataKey="drawId"
+    tick={{ fontSize: 10 }}
+    stroke="white"
+    axisLine={{ stroke: "#e1e2e3" }}
+    minTickGap={15}
+  />
+  <YAxis
+    width={40}
+    tickFormatter={formatBalance}
+    tick={{ fontSize: 10 }}
+    stroke="white"
+    domain={[network.domainMin, network.domainMax]}
+    axisLine={{ stroke: "#e1e2e3" }}
+    props={false}
+    tickCount={4}
+  />
+  <Tooltip
+    labelFormatter={(value) => `Draw ${value}`}
+    formatter={(value, name, props) => {
+      if (props.payload.drawId === undefined || props.payload.balance === undefined) {
+        return null;
+      }
+      return (
+        <div style={{ color: "black" }}>
+          <div>Draw {props.payload.drawId}</div>
+          <div>{formatBalance(props.payload.balance)}</div>
+        </div>
+      );
+    }}
+  />
+  <Area
+    type="monotone"
+    dataKey="balance"
+    stroke="#ff4fff"
+    strokeWidth={1}
+    dot={false}
+    fill="url(#areaGradient)"
+  />
+</AreaChart>
 
-            style={{ backgroundColor: 'black' }}
-          >
-            <XAxis 
-            width = {50}
-            dataKey="drawId" 
-            tick={{ fontSize: 10 }} 
-            stroke="white" 
-            axisLine={{ stroke: '#e1e2e3' }}  
-            minTickGap={15}
-            />
-            <YAxis
-              width = {40}
-              tickFormatter={formatBalance}
-              tick={{ fontSize: 10  }}
-              stroke="white"
-              domain={[network.domainMin, network.domainMax]}
-              axisLine={{ stroke: '#e1e2e3' }} 
-              props={false}
-              tickCount = {4}
-            />
-            {/* <CartesianGrid strokeDasharray="3 3" /> */}
-            {/* <Tooltip labelFormatter={(value) => `Draw ${value}`} /> */}
-            
-            {/* <Tooltip label="" formatter={(value, name, props) => {
-            if (props.payload.drawId === undefined || props.payload.balance === undefined) {
-              return null;
-            }
-            return (
-              <div style={{ color: 'black' }}>
-                <div>Draw {props.payload.drawId}</div>
-                <div>{formatBalance(props.payload.balance)}</div>
-              </div>
-            );
-          }}/> */}
- 
-            {/* <Legend wrapperStyle={{ fontSize: 8 }} /> */}
-            <Line
-            type="monotone"
-            dataKey="balance"
-            stroke="#ff4fff"
-            strokeWidth={1}
-            dot = {false}
-            label={false}
-            // activeDot={{ r: 4 }}
-          />
-          </LineChart>
         </div>
       );
     });
